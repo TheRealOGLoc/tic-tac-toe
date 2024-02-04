5136c7f7988f628b636c47ba872decdf871521ef
TheRealOGLoc <lyc990312@hotmail.com>
2024-02-04 19:10:02 +0800

    Finish the js part

--- js/main.js
+++ js/main.js
@@ -1,13 +1,134 @@
 /*----- constants -----*/
+const chess = {
+    x: "X",
+    o: "O"
+}
 
+/*----- state variables -----*/
+const state = {
+    currentPlayer: 1,
+    winner: null,
+    tie: false,
+    board: ["", "", "", "", "", "", "", "", ""]
+}
 
-	/*----- state variables -----*/
+/*----- cached elements  -----*/
+const container = document.querySelector(".container");
+const info = document.querySelector(".info");
+const restart = document.querySelector(".restart");
+const player = document.querySelector(".player");
 
+/*----- event listeners -----*/
+restart.addEventListener("click", init);
 
-	/*----- cached elements  -----*/
+for (let i = 0; i < container.children.length; i++) {
+    container.children[i].addEventListener("click", selectChess);
+}
 
+/*----- functions -----*/
+function init() {
+    resetCell();
+    state.currentPlayer = 1;
+    state.winner = null;
+    state.tie = false;
+    resetInfo();
+    render();
+}
 
-	/*----- event listeners -----*/
+function selectChess(evt) {
+    if (state. winner === null) {
+        evt.preventDefault();
+        const cell = evt.target;
+        if (state.board[Number(cell.id[5]) - 1] === "") {
+            if (state.currentPlayer === 1) {
+                state.board[Number(cell.id[5]) - 1] = chess.x;
+            } else {
+                state.board[Number(cell.id[5]) - 1] = chess.o;
+            }
+        }
+        const winner = checkWinner(state.board);
+        if (winner !== null) {
+            if (winner === "X") {
+                state.winner = 1;
+            } else {
+                state.winner = 2;
+            }
+        }
+        changeCurrentPlayer();
+        checkTie();
+        render();
+    }
+}
 
+function resetInfo() {
+    player.innerText = `Current Player: Player ${state.currentPlayer}`;
+    info.innerText = "";
+}
 
-	/*----- functions -----*/
+function render() {
+    player.innerText = `Current Player: Player ${state.currentPlayer}`;
+    for (let i = 0; i < container.children.length; i++) {
+        container.children[i].innerText = state.board[i]; 
+    }
+    if (state.winner !== null) {
+        info.innerText = `The winner is Player ${state.winner} !`;
+    } else if (state.tie === true) {
+        info.innerText = `Tie!`;
+    }
+}
+
+function resetCell() {
+    for (let i = 0; i < state.board.length; i++) {
+        state.board[i] = "";
+    }
+}
+
+function changeCurrentPlayer() {
+    if (state.winner === null) {
+        if (state.currentPlayer === 1) {
+            state.currentPlayer = 2;
+        } else {
+            state.currentPlayer = 1;
+        }
+    }
+}
+
+function checkTie() {
+    let count = 0;
+    state.board.forEach(function(cell) {
+        if (cell !== "") {
+            count += 1;
+        }
+    })
+    if (state.winner === null && count === 9) {
+        state.tie = true;
+    }
+}
+
+function checkWinner(board) {
+    const twoDBoard = [
+        rowOne = board.slice(0, 3),
+        rowTwo = board.slice(3, 6),
+        rowThree = board.slice(6, 9),
+    ]
+    for (let i = 0; i < 3; i++) {
+        if (twoDBoard[i][0] === twoDBoard[i][1] && twoDBoard[i][1] === twoDBoard[i][2] && twoDBoard[i][0] !== "") {
+            return twoDBoard[i][0];
+        }
+    }
+
+    for (let i = 0; i < 3; i++) {
+        if (twoDBoard[0][i] === twoDBoard[1][i] && twoDBoard[1][i] === twoDBoard[2][i] && twoDBoard[0][i] !== "") {
+            return twoDBoard[0][i];
+        }
+    }
+
+    if (((twoDBoard[0][0] === twoDBoard[1][1] && twoDBoard[1][1] === twoDBoard[2][2])
+        || (twoDBoard[2][0] === twoDBoard[1][1] && twoDBoard[1][1] === twoDBoard[0][2]))
+        && twoDBoard[1][1] !== "") {
+        return twoDBoard[1][1];
+    }
+    return null;
+}
+
+init();
